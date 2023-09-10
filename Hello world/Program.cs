a367ac938b7f253b5044d7e15b757ffb509b0b4a
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-09-10 19:33:34 +0100

    string operations

--- Hello world/Program.cs
+++ Hello world/Program.cs
@@ -9,3 +9,11 @@ Console.Write("You wrote your first lines of code");
 Console.WriteLine(0.25F); // create a float by appending F to the end of the number
 Console.WriteLine(2.625);  // without a suffix, c# defaults to a double
 Console.WriteLine(12.39816m);  // a value of decimal type
+
+// A verbatim string literal will keep all whitespace and characters without the need to escape the backslash.
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+
+// Combine verbatim literals and string interpolation
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
