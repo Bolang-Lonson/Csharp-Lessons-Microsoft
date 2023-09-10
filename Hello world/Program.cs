04df3b4f723befba3c161e5c0ec9c3101f35da5b
Bolang-Lonson <121783849+Bolang-Lonson@users.noreply.github.com>
2023-09-10 16:39:58 +0100

    a few more lines

--- Hello world/Program.cs
+++ Hello world/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Ya mami pima");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code");
+
+Console.WriteLine(0.25F); // create a float by appending F to the end of the number
+Console.WriteLine(2.625);  // without a suffix, c# defaults to a double
+Console.WriteLine(12.39816m);  // a value of decimal type
